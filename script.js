45e5a119202a148f7ca6dccc1e0319ae9593112a
Valher7 <Valentin.herman.pro@gmail.com>
2025-03-26 15:31:23 +0100

    Add function to display quotes

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ async function fetchApi() {
     }
 }
 
+function displayQuote(quote) {
+    const QUOTE = document.getElementById("quote");
+    QUOTE.innerHTML = quote.quote;
+
+    const AUTHOR = document.getElementById("author");
+    AUTHOR.textContent = quote.author;
+
+    const IMAGE = document.getElementById("image");
+    IMAGE.src = quote.photo;
+    IMAGE.alt = quote.author;
+
+    const CITATIONS = document.getElementById("number-of-quotes");
+    CITATIONS.textContent = quote.total_quotes;
+}
+
+document.getElementById("button").addEventListener("click", async function getRandomQuote() {
+    let res = await fetchApi();
+    displayQuote(res);  
+})
